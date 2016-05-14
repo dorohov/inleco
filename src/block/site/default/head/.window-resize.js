c8b2674c365c91cd5fe9de0be6a05f1f6c31a5e7
caezar47 <iss47@ya.ru>
2016-05-14 11:00:09 +0300

    add page 404

--- src/block/site/default/head/.window-resize.js
+++ src/block/site/default/head/.window-resize.js
@@ -1,15 +1,15 @@
-/*var h_window = $(window).height(), 
+var h_window = $(window).height(), 
  	w_window = $(window).width();
 
-var h_header = $('.header-site-block').outerHeight(true),
+var h_header = $('._czr__navbar-site').outerHeight(true),
 	h_footer = $('.footer-site').outerHeight(true),
-	w_step = $('._srcb__item-inner').outerWidth(true),
-	w_step_b = $('._srcb__step-item:before').outerWidth(true),
- 	h_404 = h_window - h_header - h_footer,
- 	w_step_drop = w_step + 40; 	
-	$("._srcb__dropdown-menu").css("max-width", w_step_drop);
+	//w_step = $('._srcb__item-inner').outerWidth(true),
+	//w_step_b = $('._srcb__step-item:before').outerWidth(true),
+ 	h_404 = h_window - h_header;
+ 	//w_step_drop = w_step + 40; 	
+	//$("._srcb__dropdown-menu").css("max-width", w_step_drop);
 if (w_window > 767){
-	$(".calc-block.content-site").css("min-height", h_404);
+	$("._czr__404-page .content-block__inner").css("height", h_404);
 } else {
-	$(".calc-block.content-site").removeAttr("style");
-};*/
+	$("._czr__404-page .content-block__inner").removeAttr("style");
+};
