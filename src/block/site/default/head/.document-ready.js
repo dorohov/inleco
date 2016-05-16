2c225f0915dd452c13ae59e8f0957b8d866c6fd1
caezar47 <iss47@ya.ru>
2016-05-16 14:42:32 +0300

    add products item page

--- src/block/site/default/head/.document-ready.js
+++ src/block/site/default/head/.document-ready.js
@@ -2,6 +2,8 @@ $('img').addClass('img-responsive');
 var url = window.location.pathname;
 $('.navbar-nav a[href="'+url+'"]').parent().addClass('active'); 
 $('._czr__ip-carousel .item').eq(0).addClass('active'); 
+$('._czr__pips__carousel .item').eq(0).addClass('active'); 
+$('._czr__pipa__carousel .item').eq(0).addClass('active'); 
 /*
 $('._fs__nav-item a[href="'+url+'"]').parent().addClass('active');
 $('._ncb__nav a[href="'+url+'"]').parent().addClass('active');
@@ -31,7 +33,15 @@ if (device.mobile() || device.tablet()) {
 $("nav.navbar-fixed-top").autoHidingNavbar();
 */
 
-$('._czr__in__modal-nav a').hover(
+$('._czr__anim-list .anim').hover(
+  function(){
+    $(this).addClass('in').removeClass('out');
+  },
+  function(){   
+      $(this).removeClass('in').addClass('out');
+  }
+)
+$('._czr__anim-indic .anim').hover(
   function(){
     $(this).addClass('in').removeClass('out');
   },
