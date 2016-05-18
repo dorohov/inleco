14e2e39b7130fd0acd32db829e4da68dadf7fb58
caezar47 <iss47@ya.ru>
2016-05-18 15:03:26 +0300

    add training page

--- src/block/site/default/head/.window-resize.js
+++ src/block/site/default/head/.window-resize.js
@@ -5,13 +5,16 @@ var h_header = $('._czr__navbar-site').outerHeight(true),
 	h_footer = $('.footer-site').outerHeight(true),
 	//w_step = $('._srcb__item-inner').outerWidth(true),
 	//w_step_b = $('._srcb__step-item:before').outerWidth(true),
- 	h_404 = h_window - h_header;
+ 	h_404 = h_window - h_header,
+ 	h_resize = h_window + 50;
  	//w_step_drop = w_step + 40; 	
 	//$("._srcb__dropdown-menu").css("max-width", w_step_drop);
 if (w_window > 767){
 	$("._czr__404-page .content-block__inner").css("height", h_404);
+	$("._czr__resize .content-block__inner").css("height", h_resize);
 	$("._czr__about-page .content-block__inner").css("height", h_404);
 } else {
 	$("._czr__404-page .content-block__inner").removeAttr("style");
+	$("._czr__resize .content-block__inner").removeAttr("style");
 	$("._czr__about-page .content-block__inner").removeAttr("style");
 };
